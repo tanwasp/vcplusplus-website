b0116e8394cc94a151128980327f43b3f6585afc
tanwasp <tanishwasp10@gmail.com>
2025-08-17 21:02:20 -0700

    remove build file

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"jaKpmSe5dsFAMo3BlvE1jrI00yXPjvZQVLNQffQ2Mk8=\"\n}"
--- .next/static/chunks/src_app_events_page_tsx_45a68a45._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_1b1a40ce._.js"
-],
-    source: "dynamic"
-});
